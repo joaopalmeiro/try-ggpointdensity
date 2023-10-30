64e6267eadc257a47eb5caeed82a21d9e94cf888
João Palmeiro <joaommpalmeiro@gmail.com>
2023-10-30 18:53:47 +0000

    Add scales

--- demo/NOTES.md
+++ demo/NOTES.md
@@ -9,3 +9,7 @@ npm install vue && npm install -D vite @vitejs/plugin-vue typescript vue-tsc cre
 ```bash
 npm install fast-kde
 ```
+
+```bash
+npm install d3-array d3-scale && npm install -D @types/d3-array @types/d3-scale
+```
--- demo/src/App.vue
+++ demo/src/App.vue
@@ -1,10 +1,31 @@
 <script setup lang="ts">
 // https://github.com/LKremer/ggpointdensity#readme
 import data from "./data_100.json";
+import { extent } from "d3-array";
+import { scaleLinear } from "d3-scale";
 
-console.log(data);
+type Datum = (typeof data)[number];
+
+const WIDTH = 300;
+const HEIGHT = WIDTH;
+
+const xAccessor = (d: Datum) => d.x;
+const yAccessor = (d: Datum) => d.y;
+
+const [xMin = 0, xMax = 0] = extent(data, xAccessor);
+const [yMin = 0, yMax = 0] = extent(data, yAccessor);
+
+const xScale = scaleLinear().domain([xMin, xMax]).range([0, WIDTH]).nice();
+const yScale = scaleLinear().domain([yMin, yMax]).range([HEIGHT, 0]).nice();
+
+// console.log(data);
 </script>
 
 <template>
-  <p>Hello, World!</p>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    :viewBox="`0 0 ${WIDTH} ${HEIGHT}`"
+    :width="`${WIDTH}px`"
+    :height="`${HEIGHT}px`"
+  ></svg>
 </template>
