47b825fa2380cf577787d83077f842155f706c4b
João Palmeiro <joaommpalmeiro@gmail.com>
2023-10-31 11:22:41 +0000

    Update App.vue

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -12,9 +12,9 @@ import data from "./data_100.json";
 
 type Datum = (typeof data)[number];
 
-const WIDTH = 300;
-const HEIGHT = WIDTH;
-const RADIUS = 3;
+const WIDTH: number = 300;
+const HEIGHT: number = WIDTH;
+const RADIUS: number = 3;
 
 const xAccessor = (d: Datum) => d.x;
 const yAccessor = (d: Datum) => d.y;
@@ -30,6 +30,7 @@ const yScale = scaleLinear().domain([yMin, yMax]).range([HEIGHT, 0]).nice();
 // https://github.com/LKremer/ggpointdensity/blob/a202ac73d1e18facb57acab8ea0a9b00680518d4/R/geom_pointdensity.R#L138
 // https://rdrr.io/cran/MASS/man/kde2d.html
 // https://d3js.org/d3-array/bisect
+// https://d3js.org/d3-array/sort
 // https://stackoverflow.com/questions/43359623/javascripts-equivalent-of-rs-findinterval-or-pythons-bisect-bisect-left
 // https://www.rdocumentation.org/packages/base/versions/3.6.2/topics/findInterval
 // https://observablehq.com/@uwdata/fast-kde#cell-61
@@ -43,12 +44,15 @@ const densityPoints = [...density];
 
 // console.log(data);
 // console.log(densityPoints);
+console.log([...new Set(densityPoints.map(xAccessor))]);
+console.log([...new Set(densityPoints.map(yAccessor))]);
 // console.log(density.grid());
 
 // R (w/ MASS::bandwidth.nrd): 1.839297 15.182454
 // fast-kde: 0.45982345378511363 3.7956138170092566
 // https://github.com/uwdata/fast-kde/blob/01c7eaff9a5901206f8ebe811b8c6a41f9688f7e/src/nrd.js#L3
 // https://github.com/cran/MASS/blob/7.3-60/R/hist.scott.R#L38
+// https://github.com/d3/d3-array/blob/v3.2.4/src/threshold/scott.js
 // https://en.wikipedia.org/wiki/Histogram (Scott's normal reference rule)
 // console.log(
 //   nrd(data, xAccessor),
